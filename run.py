c376b2958862989725fe414de6614220e558fda8
Andalib Iftakher <108351152+anda22101@users.noreply.github.com>
2025-01-07 09:06:19 +0600

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from flaskapp import app, db, socketio
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+
+    # Use socketio.run only in development; for production, use Gunicorn instead
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
